fd15243e015d2d63ba56091ef91f5be1e8066548
hernan colliard <hernancolliard5@gmail.com>
2025-12-12 17:29:36 -0300

    Se actualiza una ruta en App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Upload from "../components/Uploads";
-import MatchList from "./components/MatchList";
+import MatchList from "../components/MatchList";
 export default function App() {
   return (
     <div style={{ maxWidth: 900, margin: "20px auto", fontFamily: "Arial" }}>
