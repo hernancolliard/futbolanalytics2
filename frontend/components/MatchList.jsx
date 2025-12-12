66fd1ef037874d4248d016e4fd31feaaba67f105
hernan colliard <hernancolliard5@gmail.com>
2025-12-12 17:41:34 -0300

    Se corrige la ruta de importación del servicio API

--- frontend/components/MatchList.jsx
+++ frontend/components/MatchList.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import API from "../services/api";
+import API from "../src/services/api";
 export default function MatchList() {
   const [matches, setMatches] = useState([]);
   useEffect(() => {
